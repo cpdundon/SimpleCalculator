15dcda99a46f91c40b0a9aedcd76e743364ca556
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-05 22:36:11 -0500

    App skelaton up and running.  Had to work around a problem with Console.ReadLine() in the debug window.  Found documentation here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md#console-terminal-window

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int intUserInput;
+            string input = Console.ReadLine();
+            Console.WriteLine("TEST");
+            bool success = int.TryParse(input, out intUserInput);
+            int result = intUserInput * 2;
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
