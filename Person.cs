a7da8c49f4253ff2f80f8d49c508135efca545f3
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-06 22:49:49 -0500

    fire methods on class

--- Person.cs
+++ Person.cs
@@ -2,7 +2,17 @@ using System;
 
 namespace SimpleCalculator
 {
-    public class Person
+    class Person
     {
+        private int _age = 25;
+        public int getAge()
+        {
+            return _age;
+        }
+        public void setAge(int newAge)
+        {
+            _age = newAge;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,7 +15,9 @@ namespace SimpleCalculator
             Console.WriteLine(result.ToString());
 
             Person person1 = new Person();
-            Person person2 = new Person();
+            Console.WriteLine(person1.getAge());
+            person1.setAge(29);
+            Console.WriteLine(person1.getAge());
         }
     }
 }
