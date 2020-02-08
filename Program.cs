ea35ff6dd9f07ec835490b939e899818f8a5e68f
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-07 22:57:59 -0500

    Add error trapping to sample code.

--- Program.cs
+++ Program.cs
@@ -6,19 +6,39 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter an integer.");
-            int intUserInput;
-            string input = Console.ReadLine();
-            Console.WriteLine("TEST");
-            bool success = int.TryParse(input, out intUserInput);
-            int result = intUserInput * 2;
-            Console.WriteLine(result.ToString());
+            try
+            {
+                Console.WriteLine("Please enter an integer.");
+                int intUserInput = -999999;
+                string input = Console.ReadLine();
+                try {
+                    bool success = int.TryParse(input, out intUserInput);
+                    
+                    if (!success)
+                    {
+                        throw new Exception("Conversion was not successful.");
+                    }
+                    
+                    int result = intUserInput * 2;
+                    Console.WriteLine(result.ToString());
 
-            Person.greet();
-            Person person1 = new Person();
-            Console.WriteLine(person1.getAge());
-            person1.setAge(29);
-            Console.WriteLine(person1.getAge());
-        }
+                } catch (Exception ex) 
+                {
+                    Console.WriteLine("Conversion error: ", ex.Message);
+
+                }
+                
+                Person.greet();
+                Person person1 = new Person();
+                Console.WriteLine(person1.getAge());
+                person1.setAge(29);
+                Console.WriteLine(person1.getAge());
+            } catch (Exception ex) {
+                Console.WriteLine("There was an error: {0}", ex.Message);
+            } finally
+            {
+                Console.WriteLine("Deal with unhandled resources - open files etc.");
+            }
+        } 
     }
 }
