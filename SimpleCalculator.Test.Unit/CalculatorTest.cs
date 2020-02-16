fc179bb89ee6b23665cddbb8c4bc8f27caca6e3e
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-15 20:48:32 -0500

    Ok - the projects are compiling now with the test code added.  There is more to go but this is a good start...

---
+++ SimpleCalculator.Test.Unit/CalculatorTest.cs
@@ -0,0 +1,18 @@
+using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    public class CalculatorTest
+    {
+        public void AddsTwoNumbers_NonSymbol()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = 3;
+
+            
+
+        }
+    }
+}
